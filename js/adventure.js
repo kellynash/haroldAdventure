8a1132dd4ac230fc87e6132c74f1cc429c238ef2
Harold Shinsato <harold@shinsato.com>
2015-10-06 14:03:55 -0600

    getting ready for array of things

--- js/adventure.js
+++ js/adventure.js
@@ -1,45 +1,129 @@
-var holdon = new Audio('holdon.wav');
-var coon = new Audio('racoon.wav');
-var dead = new Audio('dead.wav');
+var holdon = new Audio('sounds/holdon.wav');
+var coon = new Audio('sounds/racoon.wav');
+var dead = new Audio('sounds/dead.wav');
 
 function Adventure(startX, startY, endX, endY, bound) {
-	this.yCoord = startX;
-	this.xCoord = startY;
-	this.startX = startX;
-	this.startY = startY;
-	this.endX = endX;
-	this.endY = endY;
+	this.coord = new Thing(startX, startY);
+	this.start = new Thing(startX, startY);
+	this.end = new Thing(endX, endY, this, this.endAction);
+    this.burns = new Thing(endX-1, endY-1, this, this.burnsAction);
 	this.bound = bound;
 	this.isGhost = false;
 }
 
-Adventure.prototype.mov = function(xDel, yDel) {
-	if(!this.isGhost && this.xCoord + xDel === this.endX && this.yCoord + yDel === this.endY) {
-        alert("Whuuthaa!!??");
-        hideAbe();	
-        this.xCoord += xDel;
-        this.yCoord += yDel;
-        killAbe();
-        dead.play();
+Adventure.prototype.endAction = function(xChange, yChange) {
+    this.adventure.hideAbe();
+    this.adventure.coord.x += xChange;
+    this.adventure.coord.y += yChange;
+    alert("Whuuthaa!!??");
+    this.adventure.killAbe();
+    dead.play();
+};
+
+Adventure.prototype.burnsAction = function(xChange, yChange) {
+    alert("You're fired!");
+    reset();
+}
+
+Adventure.prototype.atPosition = function(xChange, yChange, position) {
+    return this.coord.x + xChange === position.x && this.coord.y + yChange === position.y;
+};
+
+Adventure.prototype.mov = function(xChange, yChange) {
+	if(!this.isGhost && this.atPosition(xChange, yChange, this.end)) {
+        this.end.action(xChange, yChange);
 	}
-	else if ((this.yCoord + yDel) < 0 || (this.yCoord + yDel) >= this.bound) {
+    else if (!this.isGhost && this.atPosition(xChange, yChange, this.burns)) {
+        this.burns.action(xChange, yChange);
+    }
+	else if ((this.coord.x + xChange) < 0 || (this.coord.x + xChange) >= this.bound) {
         holdon.play();
         alert("D'oh!!!");
     }
-    else if ((this.xCoord + xDel) < 0 || (this.xCoord + xDel) >= this.bound) {
+    else if ((this.coord.y + yChange) < 0 || (this.coord.y + yChange) >= this.bound) {
         coon.play();
         alert("D'oh!!!");
     }
     else {
- 		hideAbe();
-        this.xCoord += xDel;
-        this.yCoord += yDel;
-		movAbe();
+ 		this.hideAbe();
+        this.coord.x += xChange;
+        this.coord.y += yChange;
+		this.movAbe();
 	};
 }
 
+Adventure.prototype.getID = function() {
+    return this.coord.x.toString() + this.coord.y.toString();
+};
+
+Adventure.prototype.getIDTarget = function() {
+    return this.end.x.toString() + this.end.y.toString();
+};
+
+Adventure.prototype.getIDBurns = function() {
+    return this.burns.x.toString() + this.burns.y.toString();
+};
+
+//Adding a function to replace the winning cell with a picture of dead Abe Simpson
+Adventure.prototype.killAbe = function () {
+    document.getElementById(this.getID()).innerHTML = this.makeImageElement("dead.jpg");
+    this.isGhost = true;
+};
+
+Adventure.prototype.placeBurns = function() {
+    var imageElement = this.makeImageElement("burns.jpg");
+    document.getElementById(this.getIDBurns()).innerHTML = imageElement;
+};
+
+Adventure.prototype.target = function() {
+    var imageElement = this.makeImageElement("retire.png");
+    document.getElementById(this.getIDTarget()).innerHTML = imageElement;
+};
+    
+Adventure.prototype.movAbe = function() {
+    if (myAdventure.isGhost) {
+        imageElement = this.makeImageElement("ghost.png");
+    } else {
+        imageElement = this.makeImageElement("small_abe.png");
+    }
+    document.getElementById(this.getID()).innerHTML = imageElement;
+};
+
+Adventure.prototype.makeImageElement = function(image) {
+    return "<img class=\"img-responsive center-block\" src=\"img/"
+        + image + "\"></img>";
+}
+
+Adventure.prototype.hideAbe = function () {
+    document.getElementById(this.getID()).innerHTML = "";
+};
+
+Adventure.prototype.generateGrid = function() {
+    for (var row = 0; row < this.bound; row++) {
+        document.write("<tr class=\"row\">\n");
+
+        for (var col = 0; col < this.bound; col++) {
+            document.write(" <td id=\"" + col + row + 
+                "\" class=\"cell\"></td>\n");
+        }
+        document.write("</tr>");
+    }
+    this.reset();
+};
+
 Adventure.prototype.reset = function() {
-    this.xCoord = this.startX;
-    this.yCoord = this.startY;
+    this.hideAbe();
+    this.coord.x = this.start.x;
+    this.coord.y = this.start.y;
     this.isGhost = false;
+    this.movAbe();
+    this.target();
+    this.placeBurns();
+}
+
+function Thing(x, y, adventure, action) {
+    this.x = x;
+    this.y = y;
+    this.adventure = adventure;
+    this.action = action;
 }
--- js/app.js
+++ js/app.js
@@ -1,37 +1,4 @@
-var adventureSize = Math.floor(3 + Math.random()*8);
-var myAdventure = new Adventure(0, 0, adventureSize-1, adventureSize-1, adventureSize);
-
-var getID = function() {
-    return myAdventure.yCoord.toString() + myAdventure.xCoord.toString();
-};
-
-var getIDTarget = function() {
-    return myAdventure.endY.toString() + myAdventure.endX.toString();
-};
-
-//Adding a function to replace the winning cell with a picture of dead Abe Simpson
-var killAbe = function () {
-    document.getElementById(getID()).innerHTML = "<img src=\"img/dead.jpg\"></img>";
-    myAdventure.isGhost = true;
-};
-
-var target = function(){
-    var imageElement = "<img class=\"img-responsive center-block\" src=\"img/retire.png\"></img>";
-    document.getElementById(getIDTarget()).innerHTML = imageElement;
-};
-    
-var movAbe = function(){
-    if (myAdventure.isGhost) {
-        imageElement = "<img src=\"img/ghost.png\"></img>";
-    } else {
-        imageElement = "<img class=\"img-responsive center-block\" src=\"img/small_abe.png\"></img>";
-    }
-    document.getElementById(getID()).innerHTML = imageElement;
-};
-
-var hideAbe = function () {
-    document.getElementById(getID()).innerHTML = "";
-};
+var myAdventure;
 
 var movDown = function() {
     myAdventure.mov(0, 1);
@@ -49,43 +16,32 @@ var movRight = function() {
     myAdventure.mov(1, 0);
 };
 
+var setup = function() {
+    var adventureSize = Math.floor(3 + Math.random()*8);
+    myAdventure = new Adventure(0, 0, adventureSize-1, adventureSize-1, adventureSize);
+    myAdventure.generateGrid();
+};
+
 var reset = function() {
     myAdventure.reset();
-    movAbe();
-    target();
-};
+}
 
-var generateGrid = function() {
-    for (var row = 0; row < myAdventure.bound; row++) {
-        document.write("<tr class=\"row\">\n");
-
-        for (var col = 0; col < myAdventure.bound; col++) {
-            document.write("  <td id=\"" + row + col + 
-                "\" class=\"cell\"></td>\n");
+    document.onkeydown = function(e) {
+        switch (e.keyCode) {
+            case 37:
+                movLeft();
+                break;
+            case 38:
+                movUp();
+                break;
+            case 39:
+                movRight();
+                break;
+            case 40:
+                movDown();
+                break;
+            case 32:
+                reset();
+                break;
         }
-        document.write("</tr>");
-    }
-    reset();
-};
-
-
-document.onkeydown = function(e) {
-    switch (e.keyCode) {
-        case 37:
-            movLeft();
-            break;
-        case 38:
-            movUp();
-            break;
-        case 39:
-            movRight();
-            break;
-        case 40:
-            movDown();
-            break;
-        case 32:
-            reset();
-            break;
-   }
-};
-
+    };
